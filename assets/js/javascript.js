8692473fe844fc90c5a12f1a42c4fa13d3175392
mitchelclow <mitchelclow@gmail.com>
2017-07-12 22:06:36 -0600

    added table elements in dynamically, but do not have any console errors or fields updating in table on html

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,54 +1,68 @@
-  // var config = {
-  //     apiKey: "AIzaSyA5eYKsB8T2q6rMGdKSvac6eQsWTzsZEjE",
-  //     authDomain: "fir-recent-user.firebaseapp.com",
-  //     databaseURL: "https://fir-recent-user.firebaseio.com",
-  //     storageBucket: "fir-recent-user.appspot.com"
-  //   };
-  //   firebase.initializeApp(config);
-    // Create a variable to reference the database
-    // var database = firebase.database();
-    // Initial Values
-    // var name = "";
-    // var email = "";
-    // var age = 0;
-    // var comment = "";
-    // // Capture Button Click
-    $("#add-user").on("click", function() {
+var config = {
+    apiKey: "AIzaSyBXRQg4PZ2Np-v8oJrC6QBr8V3pwh3GiY4",
+    authDomain: "shuttle-scheduler-790cb.firebaseapp.com",
+    databaseURL: "https://shuttle-scheduler-790cb.firebaseio.com",
+    projectId: "shuttle-scheduler-790cb",
+    storageBucket: "",
+    messagingSenderId: "278110168247"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+ 
+  var name = "";
+  var email = "";
+  var age = 0;
+  var comment = "";
+    // Capture Button Click
+    $("#shuttle-user").on("click", function() {
       // Don't refresh the page!
       event.preventDefault();
 
       var name = $("#shuttlename-input").val().trim();
-      var email = $("#email-input").val().trim();
-      var age = $("#age-input").val().trim();
-      var comment = $("#comment-input").val().trim();
+      var email = $("#destination-input").val().trim();
+      var age = $("#firstshuttle-input").val().trim();
+      var comment = $("#frequency-input").val().trim();
 
-      // Change the ids (html and js)
-      // Add an id for the table body
-      // Create a new table row
-      // To this table row, append <td>s with the form values
-      // Append row to table body
-
-      // database.ref().push({
-      //   name: name,
-      //   email: email,
-      //   age: age,
-      //   comment: comment
-      // });
+      database.ref().push({
+        name: name,
+        email: email,
+        age: age,
+        comment: comment
+      });
     });
     // Firebase watcher + initial loader HINT: .on("value")
-    // database.ref().on("value", function(snapshot) {
-    //   // Log everything that's coming out of snapshot
-    //   console.log(snapshot.val());
-    //   console.log(snapshot.val().name);
-    //   console.log(snapshot.val().email);
-    //   console.log(snapshot.val().age);
-    //   console.log(snapshot.val().comment);
-    //   // Change the HTML to reflect
-    //   $("#shuttlename-display").html(snapshot.val().name);
-    //   $("#email-display").html(snapshot.val().email);
-    //   $("#age-display").html(snapshot.val().age);
-    //   $("#comment-display").html(snapshot.val().comment);
-    //   // Handle the errors
-    // }, function(errorObject) {
-    //   console.log("Errors handled: " + errorObject.code);
-    // });
+    database.ref().on("child_added", function(snapshot) {
+      // Log everything that's coming out of snapshot
+      console.log(snapshot.val());
+      console.log(snapshot.val().name);
+      console.log(snapshot.val().email);
+      console.log(snapshot.val().age);
+      console.log(snapshot.val().comment);
+
+      var tableRow = $("<tr>");
+
+      var shuttleName = $("<td>");
+      var destination = $("<td>");
+      var frequency = $("<td>");
+      var nextArrival = $("<td>");
+      var minutesAway = $("<td>");
+
+      shuttleName.html(snapshot.val().name);
+      destination.html(snapshot.val().email);
+      frequency.html(snapshot.val().age);
+      nextArrival.html(snapshot.val().comment);
+      minutesAway.html("hello");
+
+      tableRow.prepend(shuttleName);
+      tableRow.prepend(destination);
+      tableRow.prepend(frequency);
+      tableRow.prepend(nextArrival);
+      tableRow.prepend(minutesAway);
+
+      $("#shuttleTable").prepend(tableRow);
+
+      // Handle the errors
+    }, function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    });
